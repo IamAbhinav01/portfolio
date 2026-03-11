7b9e1ce23740d8bbf8643271287d5ef55085b358
Abhinav <AbhinavSunil@hotmail.com>
2026-03-12 01:17:21 +0530

    Feat : Rnder

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,13 +18,16 @@ function Contact() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        'https://portfolio-backend.onrender.com/api/contact',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await response.json();
 
